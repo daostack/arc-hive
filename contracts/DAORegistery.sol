29aac405d0b361ba62ab1276578e135c62a8d925
Oren <oren.sokolowsky@gmail.com>
2019-03-24 18:27:59 +0200

    first commit

---
+++ contracts/DAORegistery.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.5.4;
+
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+contract DAORegistry is Ownable {
+
+    event Propose(address indexed _avatar);
+    event Register(address indexed _avatar, string indexed _name);
+    event UnRegister(address indexed _avatar);
+
+    mapping(string=>bool) private registry;
+
+    function propose(address _avatar) public {
+        emit Propose(_avatar);
+    }
+
+    function register(address _avatar, string memory _name) public onlyOwner {
+        require(!registry[_name]);
+        registry[_name] = true;
+        emit Register(_avatar, _name);
+    }
+
+    function unRegister(address _avatar) public onlyOwner {
+        emit UnRegister(_avatar);
+    }
+
+    //This getter is needed because Dynamically-sized keys for public mappings are not supported.
+    function isRegister(string memory _name) public view returns(bool) {
+        return registry[_name];
+    }
+
+}
