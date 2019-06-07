e641a6bee92d4ef44b4933bf46f8543ac7b4d5ef
sogasg <sogasg@gmail.com>
2019-06-07 20:15:19 +0200

    Add tests for register and unregister proposals and proposal execution

--- test/registryScheme.js
+++ test/registryScheme.js
@@ -104,10 +104,22 @@ contract("DaoRegistryScheme", accounts => {
     );
     assert.equal(tx.logs.length, 1);
     assert.equal(tx.logs[0].event, "ProposeToRegister");
-    const { _administrator, _proposalId, _address , _name} = tx.logs[0].args;
-    assert.strictEqual(_administrator.toLowerCase(), org.avatar.address.toLowerCase(), "Expect the _administrator to be the DAO's address");
-    assert.strictEqual(_address.toLowerCase(), addressToRegister.toLowerCase(), "Expect the _address to equal the address provided for registration");
-    assert.strictEqual(_name, nameToRegister, "Expect the _name to equal the name provided for registration");
+    const { _administrator, _proposalId, _address, _name } = tx.logs[0].args;
+    assert.strictEqual(
+      _administrator.toLowerCase(),
+      org.avatar.address.toLowerCase(),
+      "Expect the _administrator to be the DAO's address"
+    );
+    assert.strictEqual(
+      _address.toLowerCase(),
+      addressToRegister.toLowerCase(),
+      "Expect the _address to equal the address provided for registration"
+    );
+    assert.strictEqual(
+      _name,
+      nameToRegister,
+      "Expect the _name to equal the name provided for registration"
+    );
     assert.isNotEmpty(_proposalId);
   });
 
@@ -124,8 +136,140 @@ contract("DaoRegistryScheme", accounts => {
       addressToRegister
     );
     const { _proposalId } = tx.logs[0].args;
-    const proposal = await registryScheme.proposals.call(org.avatar.address, _proposalId);
+    const proposal = await registryScheme.proposals.call(
+      org.avatar.address,
+      _proposalId
+    );
 
     assert.exists(proposal, "There exists a proposal with this id");
+  });
+
+  it("proposeToUnregister log", async function() {
+    const { registryScheme, org } = await setup(accounts);
+
+    const addressToUnregister = "0xd3e184783ed99df8dc2c48944cd9127088983c22";
+
+    const tx = await registryScheme.proposeToUnregister(
+      org.avatar.address,
+      addressToUnregister
+    );
+    assert.equal(tx.logs.length, 1);
+    assert.equal(tx.logs[0].event, "ProposeToUnregister");
+    const { _administrator, _proposalId, _address } = tx.logs[0].args;
+    assert.strictEqual(
+      _administrator.toLowerCase(),
+      org.avatar.address.toLowerCase(),
+      "Expect the _administrator to be the DAO's address"
+    );
+    assert.strictEqual(
+      _address.toLowerCase(),
+      addressToUnregister.toLowerCase(),
+      "Expect the _address to equal the address provided for registration"
+    );
+    assert.isNotEmpty(_proposalId);
+
+    const proposal = await registryScheme.proposals.call(
+      org.avatar.address,
+      _proposalId
+    );
+
+    assert.exists(proposal, "There exists a proposal with this id");
+  });
+
+  it("propose to register and execute registration", async function() {
+    const {
+      registryScheme,
+      org,
+      registrySchemeParams,
+      daoRegistry
+    } = await setup(accounts);
+
+    // the registering entity
+    const nameToRegister = "dOrg";
+    const addressToRegister = "0xd3e184783ed99df8dc2c48944cd9127088983c22";
+
+    const tx = await registryScheme.proposeToRegister(
+      org.avatar.address,
+      nameToRegister,
+      addressToRegister
+    );
+    const { _proposalId } = tx.logs[0].args;
+    assert.isFalse(await daoRegistry.isRegister("dOrg"));
+    const vote = 1; // vote yes
+    const amount = 0; // no payment
+    const voter = helpers.NULL_ADDRESS; // not used, voting as myself
+    await registrySchemeParams.votingMachine.absoluteVote.vote(
+      _proposalId,
+      vote,
+      amount,
+      voter,
+      { from: accounts[2] }
+    );
+    assert.isTrue(await daoRegistry.isRegister("dOrg"));
+  });
+
+  it("propose to register and execute registration, then propose to unregister and execute unregistration", async function() {
+    const {
+      registryScheme,
+      org,
+      registrySchemeParams,
+      daoRegistry
+    } = await setup(accounts);
+
+    // the registering entity
+    const nameToRegister = "dOrg";
+    const addressToRegister = "0xd3e184783ed99df8dc2c48944cd9127088983c22";
+
+    // register
+    const registerTx = await registryScheme.proposeToRegister(
+      org.avatar.address,
+      nameToRegister,
+      addressToRegister
+    );
+    const { _proposalId: registerProposal } = registerTx.logs[0].args;
+    const registerVote = 1; // vote yes
+    const registerAmount = 0; // no payment
+    const registerVoter = helpers.NULL_ADDRESS; // not used, voting as myself
+    await registrySchemeParams.votingMachine.absoluteVote.vote(
+      registerProposal,
+      registerVote,
+      registerAmount,
+      registerVoter,
+      { from: accounts[2] }
+    );
+
+    assert.isTrue(
+      await daoRegistry.isRegister("dOrg"),
+      "dOrg should be registered"
+    );
+
+    // unregister
+    const unregisterTx = await registryScheme.proposeToUnregister(
+      org.avatar.address,
+      addressToRegister
+    );
+    const { _proposalId: unregisterProposal } = unregisterTx.logs[0].args;
+    const unregisterVote = 1; // vote yes
+    const unregisterAmount = 0; // no payment
+    const unregisterVoter = helpers.NULL_ADDRESS; // not used, voting as myself
+
+    await registrySchemeParams.votingMachine.absoluteVote.vote(
+      unregisterProposal,
+      unregisterVote,
+      unregisterAmount,
+      unregisterVoter,
+      { from: accounts[2] }
+    );
+
+    const allEvents = await daoRegistry.getPastEvents("allEvents", { fromBlock: 0, toBlock: "latest" });
+    const unregisterEvent = allEvents[3];
+
+    assert.equal(unregisterEvent.event, "UnRegister");
+    const { _avatar } = unregisterEvent.args;
+    assert.equal(
+      _avatar.toLowerCase(),
+      addressToRegister.toLowerCase(),
+      "the unregister event should be for the correct address"
+    );
   });
 });
